b8875b1cc1df71765cdc5f1ba58f150ac0bb4bcb
Rahalainen <rahalainen@gmail.com>
2024-05-13 20:44:06 +0300

    Add borderless fullscreen mode

--- LinuxEyeZoomMacro.sh
+++ LinuxEyeZoomMacro.sh
@@ -1,17 +1,19 @@
 ## Simple Eye Zoom Macro for Linux users (X11)
-# Author:       Rahalainen (mc, discord, github)
-# Version:      0.1.1
-# Usage:        Bind this script to a keybind shortcut to toggle the zoom whilst the Minecraft window is active.
+# Author:       Rahalainen
+# Version:      0.1.2
+# Usage:        Bind this script to a keybind to toggle the zoom while the Minecraft window is active.
 #               Run with 'debug' argument to write logs to /tmp/LinuxEyeZoomMacro.log
 # Requirements: X11 environment (tested on KDE Plasma 6.0.4), xdotool, wmctrl
 
 #!/bin/env bash
 
 ## SET SCREEN WIDTH AND HEIGHT HERE
-## recommended zoom ~ 4x
+## recommended zoom ~ 4-6x
+## set BORDERLESS=1 to use borderless fullscreen (make sure to use windowed mode before toggling)
 SCREEN_WIDTH=3440
 SCREEN_HEIGHT=1440
 ZOOM_FACTOR=4
+BORDERLESS=0
 
 # Check if the script is run with 'debug' argument
 if [ "$1" = "debug" ]; then
@@ -20,32 +22,45 @@ if [ "$1" = "debug" ]; then
 fi
 
 # Get active window and minecraft window IDs
-active_window=$(xdotool getwindowfocus)
-mc_window=$(xdotool search --class minecraft)
-echo "Active window: $active_window"
-echo "Minecraft window: $mc_window"
+ACTIVE_WINDOW=$(xdotool getwindowfocus)
+MC_WINDOW=$(xdotool search --class minecraft)
+echo "Active window: $ACTIVE_WINDOW"
+echo "Minecraft window: $MC_WINDOW"
 
 # Exit if Minecraft is not the active window
-if [ "$active_window" != "$mc_window" ]; then
+if [ "$ACTIVE_WINDOW" != "$MC_WINDOW" ]; then
+    echo -e "\nMinecraft is not the active window! Exiting..."
     exit
 fi
 
 # Check if zooming or unzooming
-eval $(xdotool getwindowgeometry --shell "$mc_window")
-echo -e "X: $X \nY: $Y \nwidth: $WIDTH \nheight: $HEIGHT \nscreen: $SCREEN"
-if [ "$WIDTH" = "$SCREEN_WIDTH" ] && [ "$HEIGHT" = "$SCREEN_HEIGHT" ]; then
-    # Not zoomed, unfullscreen and zoom
-    xdotool key F11
-    new_height=$(( SCREEN_HEIGHT * ZOOM_FACTOR ))
-
-    # Check if height is allowed or not
-    if (( $new_height > 16384 )); then
-        new_height="16384"
-    fi
-    wmctrl -i -r "$mc_window" -b remove,maximized_vert,maximized_horz
-    wmctrl -i -r "$mc_window" -e 0,0,$((-new_height/2 + SCREEN_HEIGHT/2)),"$SCREEN_WIDTH","$new_height"
+eval $(xdotool getwindowgeometry --shell "$MC_WINDOW")
+ZOOM_HEIGHT=$(( SCREEN_HEIGHT * ZOOM_FACTOR ))
+if (( ZOOM_HEIGHT > 16384 )); then
+    ZOOM_HEIGHT=16384
+    echo "Zoom height capped to 16384 px."
 else
-    # Zoomed, unzoom and fullscreen
-    wmctrl -i -r "$mc_window" -e 0,0,0,"$SCREEN_WIDTH","$SCREEN_HEIGHT"
-    xdotool key F11
+    echo "Zoom height: $ZOOM_HEIGHT"
+fi
+echo -e "\nWindow geometry before zooming/unzooming:"
+echo -e "X: $X \nY: $Y \nwidth: $WIDTH \nheight: $HEIGHT \nscreen: $SCREEN\n"
+
+# Zoom/unzoom
+if [ "$HEIGHT" = "$ZOOM_HEIGHT" ]; then
+    # Minecraft is zoomed, restore the window
+    echo "Unzooming to $SCREEN_WIDTH x $SCREEN_HEIGHT!"
+    wmctrl -i -r "$MC_WINDOW" -e 0,0,0,"$SCREEN_WIDTH","$SCREEN_HEIGHT"
+    if [ $BORDERLESS = 0 ]; then
+        xdotool key F11
+    fi
+else
+    # Minecraft is not zoomed, unfullscreen and zoom in
+    echo "Zooming to $SCREEN_WIDTH x $ZOOM_HEIGHT!"
+    if [ $BORDERLESS = 0 ]; then
+        xdotool key F11
+    fi
+
+    # Unmaximize the window (fixes zooming on KDE) and zoom in
+    wmctrl -i -r "$MC_WINDOW" -b remove,maximized_vert,maximized_horz
+    wmctrl -i -r "$MC_WINDOW" -e 0,0,$(( SCREEN_HEIGHT/2 - ZOOM_HEIGHT/2 )),"$SCREEN_WIDTH","$ZOOM_HEIGHT"
 fi
--- README.md
+++ README.md
@@ -2,25 +2,31 @@
 # LinuxEyeZoomMacro
 
 Minecraft window zooming script for X11 Linux users.
+
 ## Usage
 Clone or download the `LinuxEyeZoomMacro.sh` script and add the script as a custom keyboard shortcut.
 
-Edit `SCREEN_WIDTH` and `SCREEN_HEIGHT` in the script according to your screen's resolution. Modify the zoom factor (limited to 16384 px) if needed.
+Edit `SCREEN_WIDTH` and `SCREEN_HEIGHT` in the script according to your screen's resolution. Modify the zoom factor if needed (limited to 16384 px).
+
+To use borderless fullscreen, set `BORDERLESS=1` in the script. This allows Ninjabrain Bot to stay on top of the Minecraft window, even while not zoomed in. Make sure to use windowed mode before activating borderless mode.
+
+(To enable logging, add `debug` as an argument to write command output to `/tmp/LinuxEyeZoomMacro.log`)
 
 ### KDE Plasma 6 Example
 On Plasma 6, go to `System Settings` → `Keyboard` → `Shortcuts` → `Add New` → `Command or Script...` and insert the following to the prompt (modify path accordingly):
 ```
 bash <path_to_directory>/LinuxEyeZoomMacro.sh
 ```
-(To enable logging, add `debug` as an argument to write command output to `/tmp/LinuxEyeZoomMacro.log`)
 
 Add your shortcut, e.g. `Ctrl+J` to toggle the zoom while the Minecraft window is active.
+
 ## Requirements
 Tested with KDE Plasma 6.0.4 on Arch Linux, on X11.
 Required commands/packages:
 - xorg
 - xdotool
 - wmctrl
+
 ## Limitations
-- Only works on X11, hasn't been tested with Wayland
-- Script toggles fullscreen, no implementation for maximized windows
+- Only works on X11, not compatible with Wayland
+- No implementation for maximized windows or custom resolutions
